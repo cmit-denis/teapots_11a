ed8ac8251c6e44b4adcf49115b53b92cd47200c9
denchik <cmit-pahomov.denis@mail.ru>
2023-09-07 19:31:42 +0700

    05.04.2023, input-output cpp

---
+++ lesson/lesson1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+//Стол 1. Стандартный ввод вывод c++
+
+int main(){
+    int years;
+    cout << "Сколько вам лет???";
+    cin >> years;
+    cout << "Вам " << years << " лет!!!";
+    cout << "Вам years лет!!!";
+    return 0;
+}
